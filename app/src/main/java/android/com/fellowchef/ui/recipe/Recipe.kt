97e6a45e7ae9b23cbe2bdcdf6fb400c6c52687dd
Alfred <alfred.b.wong@gmail.com>
2021-06-25 14:02:40 -0400

    Add styling to the recipe detail section titles

--- app/src/main/java/android/com/fellowchef/ui/recipe/Recipe.kt
+++ app/src/main/java/android/com/fellowchef/ui/recipe/Recipe.kt
@@ -10,39 +10,39 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.android.parcel.Parcelize
 
-@Entity(tableName="recipe_table")
+@Entity(tableName = "recipe_table")
 @JsonClass(generateAdapter = true)
 @Parcelize
-data class Recipe (
+data class Recipe(
         @PrimaryKey(autoGenerate = true)
-    val id: Int,
-        @ColumnInfo(name="title")
-    val title: String,
-        @ColumnInfo(name="imageUrl")
-    @Json(name="image_url")
-    val imageUrl: String,
-        @ColumnInfo(name="tags")
-    @Json(name="tags")
-    val recipeType : List<String>,
-        @ColumnInfo(name="shortDescription")
-    @Json(name="short_description")
-    val shortDescription: String,
-        @ColumnInfo(name="longDescription")
-    @Json(name="long_description")
-    val longDescription: String,
-        @ColumnInfo(name="instructions")
-    val instructions : List<Instruction>,
-        @ColumnInfo(name="ingredients")
-    val ingredients : List<Ingredient>,
-        @ColumnInfo(name="numServings")
-    @Json(name="num_servings")
-    val numServings : Int,
-        @ColumnInfo(name="numLikes")
-    @Json(name="num_likes")
-    val numLikes: Int,
-        @ColumnInfo(name="numDislikes")
-    @Json(name="num_dislikes")
-    val numDislikes: Int,
-) :Parcelable{
+        val id: Int,
+        @ColumnInfo(name = "title")
+        val title: String,
+        @ColumnInfo(name = "imageUrl")
+        @Json(name = "image_url")
+        val imageUrl: String,
+        @ColumnInfo(name = "tags")
+        @Json(name = "tags")
+        val recipeType: List<String>,
+        @ColumnInfo(name = "shortDescription")
+        @Json(name = "short_description")
+        val shortDescription: String,
+        @ColumnInfo(name = "longDescription")
+        @Json(name = "long_description")
+        val longDescription: String,
+        @ColumnInfo(name = "instructions")
+        val instructions: List<Instruction>,
+        @ColumnInfo(name = "ingredients")
+        val ingredients: List<Ingredient>,
+        @ColumnInfo(name = "numServings")
+        @Json(name = "num_servings")
+        val numServings: Int,
+        @ColumnInfo(name = "numLikes")
+        @Json(name = "num_likes")
+        val numLikes: Int,
+        @ColumnInfo(name = "numDislikes")
+        @Json(name = "num_dislikes")
+        val numDislikes: Int,
+) : Parcelable {
 
 }
