c2fe6a8c40c5eb530f780e95b5e40b68dc5f5929
Alfred <alfred.b.wong@gmail.com>
2021-06-25 17:54:35 -0400

    Add a snackbar that notifies for network connectivity.
    - Add NetworkMonitorUtil to listen for network connectivity changes and if no network connection will display snackbar

--- app/src/main/java/android/com/fellowchef/MainActivity.kt
+++ app/src/main/java/android/com/fellowchef/MainActivity.kt
@@ -1,17 +1,35 @@
 package android.com.fellowchef
 
+import android.com.fellowchef.databinding.ActivityMainBinding
 import android.com.fellowchef.di.HomeRecipeComponent
+import android.com.fellowchef.network.ConnectionType
+import android.com.fellowchef.network.NetworkMonitorUtil
 import android.com.fellowchef.ui.home.HomeViewModel
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkInfo
+import android.net.NetworkRequest
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +39,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var homeRecipeComponent: HomeRecipeComponent
 
+    private val networkMonitor = NetworkMonitorUtil(this)
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //Use Dagger to inject dependencies
@@ -31,7 +51,7 @@ class MainActivity : AppCompatActivity() {
 
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -50,10 +70,46 @@ class MainActivity : AppCompatActivity() {
                 navView.visibility= View.VISIBLE
             }
         }
+        networkMonitor.result = { isAvailable, type ->
+            runOnUiThread {
+                when (isAvailable) {
+                    true -> {
+                        when (type) {
+                            ConnectionType.Wifi -> {
+                                Log.i("NETWORK_MONITOR_STATUS", "Wifi Connection")
+                            }
+                            ConnectionType.Cellular -> {
+                                Log.i("NETWORK_MONITOR_STATUS", "Cellular Connection")
+                            }
+                            else -> { }
+                        }
+                    }
+                    false -> {
+                        Log.i("NETWORK_MONITOR_STATUS", "No Connection")
+                        Snackbar.make(
+                                binding.root,
+                                R.string.network_error_text, Snackbar.LENGTH_INDEFINITE
+                        ).setAction(android.R.string.ok) {
+                        }.show()
+                    }
+                }
+            }
+        }
 
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.nav_host_fragment)
         return navController.navigateUp()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onResume() {
+        super.onResume()
+        networkMonitor.register()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        networkMonitor.unregister()
     }
 }
