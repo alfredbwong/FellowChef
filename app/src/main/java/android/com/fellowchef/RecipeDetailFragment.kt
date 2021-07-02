4b876b198a0ef2dc25b8980d0926ec1bd3df1f7a
Alfred <alfred.b.wong@gmail.com>
2021-07-02 00:34:46 -0400

    Add logic to adjust tab items height dynamically on recipe detail fragment

--- app/src/main/java/android/com/fellowchef/RecipeDetailFragment.kt
+++ app/src/main/java/android/com/fellowchef/RecipeDetailFragment.kt
@@ -3,7 +3,6 @@ package android.com.fellowchef
 import android.com.fellowchef.databinding.FragmentRecipeDetailBinding
 import android.com.fellowchef.ui.recipe.Recipe
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,7 +26,6 @@ class RecipeDetailFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         binding = FragmentRecipeDetailBinding.inflate(inflater)
         recipe = (activity as ViewRecipeActivity).recipe
-
         binding.lifecycleOwner = this
         binding.recipe = recipe
         return binding.root
@@ -41,6 +39,7 @@ class RecipeDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         pageAdapter = RecipeDetailPagerAdapter(this, recipe)
         viewpager = binding.viewPager
+        viewpager.adjustFragmentsOnPageChange()
         viewpager.adapter = pageAdapter
         val tabLayout = binding.tablayout
         TabLayoutMediator(tabLayout, viewpager) { tab, position ->
@@ -51,4 +50,45 @@ class RecipeDetailFragment : Fragment() {
 
         }.attach()
     }
+    // This function can sit in an Helper file, so it can be shared across your project.
+    fun updatePagerHeightForChild(view: View, pager: ViewPager2) {
+        view.post {
+            val wMeasureSpec =
+                    View.MeasureSpec.makeMeasureSpec(view.width, View.MeasureSpec.EXACTLY)
+            val hMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+            view.measure(wMeasureSpec, hMeasureSpec)
+
+            if (pager.layoutParams.height != view.measuredHeight) {
+                pager.layoutParams = (pager.layoutParams)
+                        .also { lp ->
+                            // applying Fragment Root View Height to
+                            // the pager LayoutParams, so they match
+                            lp.height = view.measuredHeight
+                        }
+            }
+        }
+    }
+
+    private fun ViewPager2.adjustFragmentsOnPageChange(){
+        this.registerOnPageChangeCallback((object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+
+                // Because the fragment might or might not be created yet,
+                // we need to check for the size of the fragmentManager
+                // before accessing it.
+                if (childFragmentManager.fragments.size > position) {
+                    val fragment = childFragmentManager.fragments.get(position)
+                    fragment.view?.let {
+                        // Now we've got access to the fragment Root View
+                        // we will use it to calculate the height and
+                        // apply it to the ViewPager2
+                        updatePagerHeightForChild(it, binding.viewPager)
+                    }
+                }
+
+            }
+        }))
+    }
 }
+
