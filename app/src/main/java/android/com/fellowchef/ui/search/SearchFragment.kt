bf02c9d8b233d01da013f2b1ed62e8c58ce6237b
Alfred <alfred.b.wong@gmail.com>
2021-07-02 13:36:05 -0400

    Add navigation search on bottom nav view and initial fragment
    - Added search button on bottom nav view
    - Added fragment class to be displayed on clicked bottom nav view button.

--- app/src/main/java/android/com/fellowchef/ui/search/SearchFragment.kt
+++ app/src/main/java/android/com/fellowchef/ui/search/SearchFragment.kt
@@ -1,4 +1,4 @@
-package android.com.fellowchef.ui.notifications
+package android.com.fellowchef.ui.search
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import android.com.fellowchef.R
 
-class NotificationsFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var searchViewModel: SearchViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        searchViewModel =
+                ViewModelProvider(this).get(SearchViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_search, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        searchViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
