93414c6ae7d8173cf9f7cca44c5f900ca688c7db
Alfred <alfred.b.wong@gmail.com>
2021-07-19 16:46:45 -0400

    Update UI for recipe detail fragment (remove tabbed pane and make into single list)
    Updated UI to remove tabbed pane layout as it required too much calculations and was not functionally correct. Re-implement as a single list with ingredients and instructions as CardView using recycler views.

--- app/src/main/java/android/com/fellowchef/RecipeDetailFragment.kt
+++ app/src/main/java/android/com/fellowchef/RecipeDetailFragment.kt
@@ -1,22 +1,19 @@
 package android.com.fellowchef
 
 import android.com.fellowchef.databinding.FragmentRecipeDetailBinding
-import android.com.fellowchef.repository.models.Status
+import android.com.fellowchef.models.IngredientsListAdapter
+import android.com.fellowchef.models.InstructionsListAdapter
 import android.com.fellowchef.ui.recipe.Recipe
 import android.com.fellowchef.ui.viewmodel.RecipeDetailViewModel
 import android.os.Bundle
-import android.transition.TransitionInflater
-import android.util.Log
 import android.view.*
-import androidx.core.app.ActivityCompat.invalidateOptionsMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 
 /**
@@ -27,7 +24,6 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class RecipeDetailFragment : Fragment() {
     private lateinit var pageAdapter: RecipeDetailPagerAdapter
-    private lateinit var viewpager: ViewPager2
     private lateinit var binding: FragmentRecipeDetailBinding
     private lateinit var recipe: Recipe
     private var isRecipeLiked: Boolean  = false
@@ -41,20 +37,33 @@ class RecipeDetailFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        recipeDetailViewModel.isRecipeLiked.observe(this, Observer {
-            isLiked ->
+        recipeDetailViewModel.isRecipeLiked.observe(this, Observer { isLiked ->
             isRecipeLiked = isLiked
             requireActivity().invalidateOptionsMenu()
 
         })
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentRecipeDetailBinding.inflate(inflater)
         recipe = args.recipe
         recipeDetailViewModel.updateReferencedRecipe(recipe)
         binding.lifecycleOwner = this
+
+
+        val ingredientAdapter = IngredientsListAdapter()
+        binding.ingredientsListView.adapter = ingredientAdapter.apply{
+            submitList(recipe.ingredients)
+        }
+
+        binding.ingredientsListView.addItemDecoration(DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL))
+        val instructionsAdapter = InstructionsListAdapter()
+        binding.instructionsListView.adapter = instructionsAdapter.apply{
+            submitList(recipe.instructions)
+        }
         binding.recipe = recipe
         setHasOptionsMenu(true)
         return binding.root
@@ -67,57 +76,7 @@ class RecipeDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         pageAdapter = RecipeDetailPagerAdapter(this, recipe)
-        viewpager = binding.viewPager
-        viewpager.adjustFragmentsOnPageChange()
-        viewpager.adapter = pageAdapter
-        val tabLayout = binding.tablayout
-        TabLayoutMediator(tabLayout, viewpager) { tab, position ->
-            if (position == 0)
-                tab.text = getString(R.string.ingredients)
-            else if (position == 1)
-                tab.text = getString(R.string.instructions)
 
-        }.attach()
-    }
-    // This function can sit in an Helper file, so it can be shared across your project.
-    fun updatePagerHeightForChild(view: View, pager: ViewPager2) {
-        view.post {
-            val wMeasureSpec =
-                    View.MeasureSpec.makeMeasureSpec(view.width, View.MeasureSpec.EXACTLY)
-            val hMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-            view.measure(wMeasureSpec, hMeasureSpec)
-
-            if (pager.layoutParams.height != view.measuredHeight) {
-                pager.layoutParams = (pager.layoutParams)
-                        .also { lp ->
-                            // applying Fragment Root View Height to
-                            // the pager LayoutParams, so they match
-                            lp.height = view.measuredHeight
-                        }
-            }
-        }
-    }
-
-    private fun ViewPager2.adjustFragmentsOnPageChange(){
-        this.registerOnPageChangeCallback((object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-
-                // Because the fragment might or might not be created yet,
-                // we need to check for the size of the fragmentManager
-                // before accessing it.
-                if (childFragmentManager.fragments.size > position) {
-                    val fragment = childFragmentManager.fragments.get(position)
-                    fragment.view?.let {
-                        // Now we've got access to the fragment Root View
-                        // we will use it to calculate the height and
-                        // apply it to the ViewPager2
-                        updatePagerHeightForChild(it, binding.viewPager)
-                    }
-                }
-
-            }
-        }))
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -128,9 +87,9 @@ class RecipeDetailFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
-            R.id.likeButton->{
+            R.id.likeButton -> {
                 recipeDetailViewModel.isRecipeLiked.value = !isRecipeLiked
-                if (isRecipeLiked){
+                if (isRecipeLiked) {
                     recipeDetailViewModel.addLikedRecipe(recipe)
                 } else {
                     recipeDetailViewModel.removeRecipeFromLiked(recipe.id)
